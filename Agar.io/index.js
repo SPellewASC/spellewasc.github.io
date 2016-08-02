d5bd68232a0702fb7f73729a643b66f96671fb7f
SPellewASC <shamroypellew@aol.com>
2016-08-02 12:28:39 -0400

    Update index.js

--- Agar.io/index.js
+++ Agar.io/index.js
@@ -41,5 +41,10 @@ function draw() {
 		background(127);
 		animation(basketball,305,570);
 		animation(hoop,305,127)
+		
+		if (mouseButton == LEFT) {
+			basketball.velocity.x = random(-5,5)
+			basketball.velocity.y = random(-5,5)
+		}
 	}
 }
