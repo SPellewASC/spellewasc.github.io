014f0a3dbdc3587f057beded14a9387daff9d144
Shamroy Pellew <shamroypellew@aol.com>
2016-08-05 08:51:10 -0400

    commit

--- Agar.io/index.js
+++ Agar.io/index.js
@@ -1,16 +1,25 @@
 
-var mode;
-var gameover;
-var score;
-var player_1;
+var mode = 0;
+var gameover = false;
+var score = 0;
+var player;
+var frame1 = "http://spellewasc.github.io/Agar.io/assets/player_model.png";
+var size = 64;
+
+function preload() {
+	player_model = loadImage(frame1);
+}
 
 function setup() {
-	score=0;
-	mode=0;
-	gameover=false;
+	player = createSprite(mouseX,mouseY,size,size);
+	player.shapeColor = color(0);
+	// player.addImage(player_model);
 }
 
 function draw() {
+
+	drawSprites();
+	
 	if (mode === 0 && gameover === false) {
 		createCanvas(610,610)
 		background(0)
@@ -33,8 +42,8 @@ function draw() {
 	if (mode === 1) {
 		createCanvas(610,610);
 		background(255);
-		player_1 = loadAnimation("assets/player_model.png");
-		animation(player_1,mouseX,mouseY);
-		draw(mouseX,mouseY);
+		player.position.x = mouseX;
+		player.position.y = mouseY;
+
 	}
 }
