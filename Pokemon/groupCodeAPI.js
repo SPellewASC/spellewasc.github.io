09082fd34bb49e2759d866412d9b3e8c37ab3492
Shamroy Pellew <shamroypellew@aol.com>
2016-08-05 16:11:55 -0400

    commit

--- Pokemon/groupCodeAPI.js
+++ Pokemon/groupCodeAPI.js
@@ -2,13 +2,13 @@
 var player;
 var loadimg;
 var url = "http://maps.googleapis.com/maps/api/staticmap?center=Rockefeller+Center,+10+Rockefeller+Plaza,+New+York,+NY+10020&zoom=15&scale=1&size=640x640&maptype=roadmap&key=AIzaSyDJXU9P8ieyia_jPLo26RSrj4tx7Kq1rg4&format=png&visual_refresh=true";
-var markerList = ["&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/025-pikachu-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/133-eevee-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/056-mankey-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/058-growlithe-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/016-pidgey-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/063-abra-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/060-poliwag-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/043-oddish-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/041-zubat-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/043-oddish-icon.png%7Cshadow:true%7C"];
-// var pikas = ["601 W 26th St. NY","500 W 30th St. NY", "551 W 26th St. NY", "685 W 26th St. NY", "600 25th St. NY"];
-var pikasList = [];
 var frame1 = "https://spellewasc.github.io/Pokemon/assets/player_model.gif";
 var direction = 90;
 var mouse = false;
 var cnv;
+var pokeID;
+var pokeimg;
+var pokemon;
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -33,20 +33,35 @@ function changeinput() {
 		}
 	}
 }
-/*
-for (var i = 0; i < pikas.length; i ++) {
-	
-	var pika = encodeURI(pikas[i]);
-	var markerStart = markerList[Math.floor(Math.random()*markerList.length)];
-	console.log(markerStart);
-	url = url + markerStart + pika;
+
+function createVariables(){
+  var accounts = [];
+
+  for (var i = 0; i <= 20; ++i) {
+      accounts[i] = "whatever";
+  }
+
+  return accounts;
 }
-*/
+
 function preload() {
+
+	var pokelist = [];
+	var pokeimgs = [];
 
 	canvas = loadImage(url);
 	player_model = loadImage(frame1);
-	
+
+	for (var i = 0; i <= 10; ++i) {
+		pokeID = Math.floor(Math.random() * (151 - 1 + 1) + 1);
+		pokelist.push(pokeID);
+		var base = "http://pokeapi.co/media/sprites/pokemon/";
+		var png = ".png";
+		pokeimg = base + pokelist[i] + png;
+		loadImage(pokeimg)
+		console.log(pokeimg)
+	}	
+
 }
 
 function setup() {
@@ -56,7 +71,10 @@ function setup() {
 	image(canvas,0,0);
 	player = createSprite(320,320,64,64);
 	player.addImage(player_model);
-	canvas.parent('sketch-holder');
+	for (var i = 0; i <= 10; ++i) {
+		pokemon[i] = createSprite(Math.random() * 640, Math.random() * 640, 1, 1)
+		player.addImage(pokeimg)
+	}
 
 }
 
