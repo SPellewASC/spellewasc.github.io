29097dd95593228b64934bfbd2339c8ec03ecb57
SPellewASC <shamroypellew@aol.com>
2016-08-05 10:55:31 -0400

    Update groupCodeAPI.js

--- Pokemon/groupCodeAPI.js
+++ Pokemon/groupCodeAPI.js
@@ -3,7 +3,7 @@ var player;
 var loadimg;
 var url = "http://maps.googleapis.com/maps/api/staticmap?center=Rockefeller+Center,+10+Rockefeller+Plaza,+New+York,+NY+10020&zoom=13&scale=2&size=640x640&maptype=roadmap&key=AIzaSyDJXU9P8ieyia_jPLo26RSrj4tx7Kq1rg4&format=png&visual_refresh=true";
 var markerList = ["&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/025-pikachu-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/133-eevee-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/056-mankey-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/058-growlithe-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/016-pidgey-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/063-abra-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/060-poliwag-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/043-oddish-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/041-zubat-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/043-oddish-icon.png%7Cshadow:true%7C"];
-var pikas = ["601 W 26th St. NY","500 W 30th St. NY", "551 W 26th St. NY", "685 W 26th St. NY", "600 25th St. NY"];
+// var pikas = ["601 W 26th St. NY","500 W 30th St. NY", "551 W 26th St. NY", "685 W 26th St. NY", "600 25th St. NY"];
 var pikasList = [];
 var frame1 = "https://spellewasc.github.io/Pokemon/assets/player_model.gif";
 var direction = 90;
@@ -22,7 +22,7 @@ function changeinput() {
 		}
 	}
 }
-
+/*
 for (var i = 0; i < pikas.length; i ++) {
 	
 	var pika = encodeURI(pikas[i]);
@@ -30,7 +30,7 @@ for (var i = 0; i < pikas.length; i ++) {
 	console.log(markerStart);
 	url = url + markerStart + pika;
 }
-
+*/
 function preload() {
 
 	canvas = loadImage(url);
