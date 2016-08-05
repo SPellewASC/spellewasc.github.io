9a7a577db7c8fe8f02320bc6233b1040ad6f9d26
Shamroy Pellew <shamroypellew@aol.com>
2016-08-05 11:50:08 -0400

    commit

--- Pokemon/groupCodeAPI.js
+++ Pokemon/groupCodeAPI.js
@@ -1,13 +1,25 @@
 
 var player;
 var loadimg;
-var url = "http://maps.googleapis.com/maps/api/staticmap?center=601%20W.%2026th%20ST.%20NY&zoom=16&size=1200x600&key=AIzaSyDJXU9P8ieyia_jPLo26RSrj4tx7Kq1rg4&visual_refresh=true";
+var url = "http://maps.googleapis.com/maps/api/staticmap?center=Rockefeller+Center,+10+Rockefeller+Plaza,+New+York,+NY+10020&zoom=15&scale=1&size=640x640&maptype=roadmap&key=AIzaSyDJXU9P8ieyia_jPLo26RSrj4tx7Kq1rg4&format=png&visual_refresh=true";
 var markerList = ["&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/025-pikachu-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/133-eevee-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/056-mankey-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/058-growlithe-icon.png%7Cshadow:true%7C", "&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/016-pidgey-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/063-abra-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/060-poliwag-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/043-oddish-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/041-zubat-icon.png%7Cshadow:true%7C","&markers=icon:http://megaicons.net/static/img/icons_sizes/388/1147/64/043-oddish-icon.png%7Cshadow:true%7C"];
-var pikas = ["601 W 26th St. NY","500 W 30th St. NY", "551 W 26th St. NY", "685 W 26th St. NY", "600 25th St. NY"];
+// var pikas = ["601 W 26th St. NY","500 W 30th St. NY", "551 W 26th St. NY", "685 W 26th St. NY", "600 25th St. NY"];
 var pikasList = [];
 var frame1 = "https://spellewasc.github.io/Pokemon/assets/player_model.gif";
 var direction = 90;
 var mouse = false;
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
 
 function changeinput() {
 	if (mouse === false) {
@@ -22,7 +34,7 @@ function changeinput() {
 		}
 	}
 }
-
+/*
 for (var i = 0; i < pikas.length; i ++) {
 	
 	var pika = encodeURI(pikas[i]);
@@ -30,23 +42,30 @@ for (var i = 0; i < pikas.length; i ++) {
 	console.log(markerStart);
 	url = url + markerStart + pika;
 }
-
+*/
 function preload() {
 
 	canvas = loadImage(url);
 	player_model = loadImage(frame1);
+	
 }
 
 function setup() {
 
-	createCanvas(600,300);
+    cnv = createCanvas(640, 640);
+    centerCanvas();
 	image(canvas,0,0);
-	player = createSprite(300,150,64,64);
+	player = createSprite(320,320,64,64);
 	player.addImage(player_model);
 
 }
 
+function windowResized() {
+  centerCanvas();
+}
+
 function draw() {
+	
 	if (mouse === true) {
 	}
 
@@ -72,4 +91,3 @@ function draw() {
 	drawSprites();
 
 }
-
