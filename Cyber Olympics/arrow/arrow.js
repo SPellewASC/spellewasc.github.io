f71841204b677fecd99e648d37f64591eab9fae7
Shamroy Pellew <shamroypellew@aol.com>
2016-08-16 11:07:05 -0400

    commit

--- Cyber Olympics/arrow/arrow.js
+++ Cyber Olympics/arrow/arrow.js
@@ -3,7 +3,6 @@ var w = 1350;
 var h = 600;
 var scene_h = h*2
 var game = false;
-var background;
 var cnv;
 // var time;
 var millisecond;
@@ -462,3 +461,13 @@ function draw() {
 		score_counter();
 	}
 }
+
+$('body').toggleClass('loaded');
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
