cc255810c7077e7196772368e04326baff87d0da
SPellewASC <shamroypellew@aol.com>
2016-08-19 13:40:20 -0400

    Update selections.js

--- portfolio/selections.js
+++ portfolio/selections.js
@@ -2,3 +2,7 @@
 function pokemon_js() {
   window.open("https://spellewasc.github.io/portfolio/Pokemon_groupCodeAPI/groupCodeAPI.html");
 }
+
+function olympics_js() {
+  window.open("https://cfloresasc.github.io/pls/Olympic.html");
+}
