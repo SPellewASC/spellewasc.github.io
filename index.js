275ce3d1d07745f11796340fc92be3acc47ab441
SPellewASC <shamroypellew@aol.com>
2016-08-22 15:48:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function froth() {
         "x": foam_x,
         "y": foam_y,
         "radius": 12,
-        "fill": "#D9B310"
+        "fill": "#C5C1C0"
     }
     if (canvas.getContext) {
         context.beginPath();
