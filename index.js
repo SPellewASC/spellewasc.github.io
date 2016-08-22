ebf9fb271d9ee69d0815de02bb85b3c23a7fbee6
SPellewASC <shamroypellew@aol.com>
2016-08-22 15:01:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,43 +2,30 @@ var canvas_width = screen.availWidth;
 var canvas_height = screen.availHeight;
 //The 30 and 90 above are arbitrary figures to allow for other DIVS on the page
 var canvas_div = window.document.getElementById("Canvas_Container");
-canvas_div.innerHTML = "<canvas id='Canvas_1' width='"+canvas_width+"' height='"+canvas_height+"'></canvas>";
-
-/*
-
-var hypertext = document.getElementById("hypertext");
-
-*/
+canvas_div.innerHTML = "<canvas id='Canvas_1' width='" + canvas_width + "' height='" + canvas_height + "'></canvas>";
 
 var canvas = document.getElementById("Canvas_1");
-var ctx = canvas.getContext("2d");
-var raf;
+var context = canvas.getContext("2d");
 
-// Get the modal
-var modal = document.getElementById('modal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("button");
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+var init = function() {
+    for (var i = 0; i <= 200; i++) {
+        requestAnimationFrame(froth);
     }
 }
 
-function init() {
-    var canvas = document.getElementById("Canvas_1");
+function froth() {
+    var foam_x = Math.random() * canvas_width
+    var foam_y = Math.random() * canvas_height
+    var foam = {
+        "x": foam_x,
+        "y": foam_y,
+        "radius": 20,
+        "fill": "#5bc0de"
+    }
     if (canvas.getContext) {
-          var context = canvas.getContext("2d");
-	  ctx.beginPath();
-	  ctx.arc(95,50,20,0,2*Math.PI);
-	  ctx.fillStyle = "#5bc0de";
-	  ctx.fill();
+        context.beginPath();
+        context.arc(foam.x, foam.y, foam.radius, 0, 2 * Math.PI);
+        context.fillStyle = foam.fill;
+        context.fill();
     }
 }
